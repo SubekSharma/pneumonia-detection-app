271a7ab31d929494d056d43fb8a5d028bca527b5
Subek Sharma <subeksharmaofficial@gmail.com>
2024-01-21 21:40:30 +0545

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ def preprocess_image(img):
     img = image.load_img(img, target_size=img_size)
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
-    img_array = preprocess_input(img_array)
+    
     return img_array
 
 # Function to make predictions
